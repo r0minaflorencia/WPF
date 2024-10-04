0d04a36ad5a43c6e63baf57435ef6a9a1e1230f7
Romina Avalos <rflorencia.avalos@gmail.com>
2024-10-04 00:33:05 -0300

    3rd commit

--- GrupoN/DB/DB.cs
+++ GrupoN/DB/DB.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,10 +17,35 @@ namespace GrupoN.DB
 
         public DB()
         {
-            _connection = @"Server=DESKTOP-PRJRN6F\DBSERVER; Database=GrupoN; Trusted_Connection=true;";
+            _connection = @"Server=DESKTOP-PRJRN6F\DBSERVER; Database=GrupoN;";
         }
 
+        internal ObservableCollection<ProfesorModel> Get()
+        {
+            ObservableCollection<ProfesorModel> lastResult = new ObservableCollection<ProfesorModel>();
 
+            string query = "SELECT * FROM profesores";
+
+            using (SqlConnection connect = new SqlConnection(Connection))
+            {
+                connect.Open();
+                SqlCommand cmd = new SqlCommand(query, connect);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    lastResult.Add(new ProfesorModel()
+                    {
+                        Id = (int)reader["ID"],
+                        Nombre = (string)reader["NOMBRE"],
+                        Apellido = (string)reader["APELLIDO"],
+                        Especialidad = (string)reader["ESPECIALIDAD"]
+                    });
+                }
+                reader.Close();
+                connect.Close();
+            }
+
+        }
 
 
     }
--- GrupoN/Models/ProfesorModel.cs
+++ GrupoN/Models/ProfesorModel.cs
@@ -1,30 +1,72 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace GrupoN.Models { 
 	public class ProfesorModel()
 	{
-        // atributos + get&set
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Especialidad { get; set; }
+        
+            // encapsulamiento:
+            private int _id;
+            private string _nombre;
+            private string _apellido;
+            private string _especialidad;
 
-        // Constructor por defecto
-        public Profesor() { }
+            public int Id
+            {
+                get { return _id; }
+                set
+                {
+                    if (value <= 0)
+                    {
+                        throw new ArgumentException("El Id debe ser mayor a 0.");
+                    }
+                    _id = value;
+                }
+            }
 
-        // Constructor con parámetros
-        public Profesor(int Id, string Nombre, string Apellido, string Especialidad)
-        {
-            Id = Id;    
-            Nombre = Nombre;
-            Apellido = Apellido;
-            Especialidad = Especialidad;
-        }
+            public string Nombre
+            {
+                get { return _nombre; }
+                set
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                    {
+                        throw new ArgumentNullException(nameof(Nombre), "El nombre no puede estar vacío.");
+                    }
+                    _nombre = value;
+                }
+            }
 
-        // Método para mostrar información de la persona
-        public void MostrarInfo()
-        {
-            Console.WriteLine($"Nombre: {Nombre}, Apellido: {Apellido}, Especialidad: {Especialidad}");
+            public string Apellido
+            {
+                get { return _apellido; }
+                set
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                    {
+                        throw new ArgumentNullException(nameof(Apellido), "El apellido no puede estar vacío.");
+                    }
+                    _apellido = value;
+                }
+            }
+
+            public string Especialidad
+            {
+                get { return _especialidad; }
+                set
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                    {
+                        throw new ArgumentNullException(nameof(Especialidad), "La especialidad no puede estar vacía.");
+                    }
+                    _especialidad = value;
+                }
+            }
+
+            public override string? ToString()
+            {
+                return base.ToString();
+            }
         }
 
     }
